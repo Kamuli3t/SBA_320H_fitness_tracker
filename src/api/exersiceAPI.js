a0e0860bc59b5e07948b2f323a8941756d7cf91c
kamuli3t <kamuli3t@gmail.com>
2025-03-17 09:48:02 -0700

    update .gitignore to include .env file and refactor exerciseAPI to use environment variable for API key

--- src/api/exersiceAPI.js
+++ src/api/exersiceAPI.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const exerciseAPI = axios.create({
   baseURL: "https://exercisedb.p.rapidapi.com/exercises/bodyPart",
   headers: {
-    "x-rapidapi-key": "03c3ab99c7msha9a4df794bf36efp101838jsn788aed25c157",
+    "x-rapidapi-key": import.meta.env.VITE_EX_API,
     "x-rapidapi-host": "exercisedb.p.rapidapi.com",
   },
   params: { limit: "10" },
